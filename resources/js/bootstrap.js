edeac8e77ad7a79722050f171be3924e4e1e14b9
Derek Myers <arcticpro@gmail.com>
2019-10-10 19:52:34 -0500

    added setup of axios base url to app url

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -21,6 +21,7 @@ try {
 
 window.axios = require('axios');
 
+window.axios.defaults.baseURL = process.env.MIX_APP_URL;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
