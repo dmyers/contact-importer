8850bff6950a4c066651620223387308fdb34f09
Derek Myers <arcticpro@gmail.com>
2019-12-11 16:15:32 -0600

    added icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,11 +13,11 @@ window.Vue = require('vue');
 Vue.use(BootstrapVue);
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheck, faChevronRight, faArrowRight, faLongArrowAltRight, faUsers } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faCheck, faChevronRight, faArrowRight, faLongArrowAltRight, faUsers, faBullhorn } from '@fortawesome/free-solid-svg-icons'
 import { faFileExcel } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faCheck, faChevronRight, faArrowRight, faLongArrowAltRight, faUsers, faFileExcel);
+library.add(faPlus, faCheck, faChevronRight, faArrowRight, faLongArrowAltRight, faUsers, faBullhorn, faFileExcel);
 
 Vue.component('fa-icon', FontAwesomeIcon);
 
