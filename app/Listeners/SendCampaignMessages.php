d01580a4328bd699ca256c8e2e6ac91ed0dd107f
Derek Myers <arcticpro@gmail.com>
2019-12-11 19:04:08 -0600

    added parsing variables out of body when sending messages

--- app/Listeners/SendCampaignMessages.php
+++ app/Listeners/SendCampaignMessages.php
@@ -22,10 +22,19 @@ class SendCampaignMessages implements ShouldQueue
 
         Contact::chunk(500, function ($contacts) use ($campaign) {
             $contacts->each(function ($contact) use ($campaign) {
+                $body = $campaign->message;
+
+                if (strpos($body, '{FNAME}') !== false) {
+                    $body = str_replace('{FNAME}', $contact->first_name, $body);
+                }
+                if (strpos($body, '{LNAME}') !== false) {
+                    $body = str_replace('{LNAME}', $contact->first_name, $body);
+                }
+
                 $message = Message::create([
                     'contact_id'  => $contact->id,
                     'campaign_id' => $campaign->id,
-                    'body'        => $campaign->message,
+                    'body'        => $body,
                 ]);
 
                 SendContactMessage::dispatch($message);
